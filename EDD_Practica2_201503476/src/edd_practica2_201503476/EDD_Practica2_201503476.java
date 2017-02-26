5104674208b315c25ccd9da155e480a1eebea228
ricar <ricar@10.154.130.103>
2017-02-26 16:28:00 -0600

    Primera Actualización: INTERFAZ DE USUARIO

---
+++ EDD_Practica2_201503476/src/edd_practica2_201503476/EDD_Practica2_201503476.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd_practica2_201503476;
+
+/**
+ *
+ * @author ricar
+ */
+public class EDD_Practica2_201503476 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
